a46b75257e1e9397c01903de0d94c7a73df25d26
YuHanul <70747418+dbrjsdn1220@users.noreply.github.com>
2023-08-04 15:36:55 +0900

    Update 0.5.3
    Release note 0.5.3
    단순한 시각효과를 추가하였습니다.

--- website/resource/script.js
+++ website/resource/script.js
@@ -20,12 +20,12 @@ function volumeControl(number) //소리 조절하는 함수
     muteCheck = 0;
     volumeImage.src = "icons/nonMute.png";
   }
-  for(i=10; i>0; i--){
+  for(i=1; i<=10; i++){
     var volumeSet = document.getElementById("volume"+i);
-    if(i>=number){
+    if(i<=number){
       volumeSet.style="background-color: cornflowerBlue";
     }
-    else if(i<number){
+    else if(i>number){
       volumeSet.style="background-color: white";
     }
   }
@@ -35,31 +35,38 @@ function volumeControl(number) //소리 조절하는 함수
 function iframeSelect(selector) // 메뉴 선택 시 웹사이트 이동
 {
   var iframe = document.getElementById("iframe");
+  var imageChange = document.getElementById("menu"+selector);
   var max = 4; // 현재 누를 수 있는 메뉴의 개수
 
-  for(i=1; i<=4; i++){ //선택된 메뉴 색깔 변경
-    var colorChange = document.getElementById("menu"+i);
-    if(i==selector){
-      colorChange.style = "background-color: cornflowerBlue";
-    }
-    else{
-      colorChange.style = "background-color: none";
-    }
-  }
-  //해당하는 웹페이지로 이동
+  //해당하는 웹페이지로 이동 및 색 변경
   if(selector==1){ // 대시보드
-    iframe.src = "html/main_test.html";
+    iframe.src = "html/dashboard.html";
+    imageChange.style = 
+    "background: url('icons/menuDashboard_white.png') 5% 50% no-repeat cornflowerBlue; color: white";
   }
   else if(selector==2){ // 사용자
     iframe.src = "html/user.html";
+    imageChange.style = 
+    "background: url('icons/menuUser_white.png') 5% 50% no-repeat cornflowerBlue; color: white";
   }
   else if(selector==3){ // 알레르기
-    iframe.src = "https://www.allergy.or.kr/";
+    iframe.src = "html/allergy.html";
+    imageChange.style = 
+    "background: url('icons/menuAllergy_white.png') 5% 50% no-repeat cornflowerBlue; color: white";
   }
   else if(selector==4){ // 요리법
     iframe.src = "https://www.10000recipe.com/";
+    imageChange.style = 
+    "background: url('icons/menuHowtocook_white.png') 5% 50% no-repeat cornflowerBlue; color: white";
+  }
+
+  for(i=1; i<=4; i++){ //선택되지 않은 메뉴 색 초기화
+    var colorChange = document.getElementById("menu"+i);
+    if(i!=selector){
+      colorChange.style = "background-color: none";
+    }
   }
 }
 
-window.onload = volumeControl(6); //초기 음량 설정
+window.onload = volumeControl(5); //초기 음량 설정
 window.onload = iframeSelect(1); //초기 페이지 접속
