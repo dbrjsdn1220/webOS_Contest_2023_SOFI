ba543404abc31326993b3fb3fb3e5b81a47d6f74
YuHanul <dbrjsdn1220@gmail.com>
2023-08-07 20:15:09 +0900

    Update server.js

--- website/server.js
+++ website/server.js
@@ -1,11 +1,55 @@
-const express = require("express");
-const server = express();
-const port = 3000;
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-server.get('/', (req, res) => {
-    res.send('Hello, world!');
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        const url = req.url;
+        
+        if (url === '/' || url === '/start_sofi.html') {
+            const filePath = path.join(__dirname, 'start_sofi.html');
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(data);
+                }
+            });
+        } else if (url === '/index.html') {
+            const indexPath = path.join(__dirname, 'index.html');
+            fs.readFile(indexPath, (err, data) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                    res.end('Internal Server Error');
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(data);
+                }
+            });
+        } else if (url === '/resource/icons/sofi.png') {
+            const imagePath = path.join(__dirname, 'resource', 'icons', 'sofi.png');
+            fs.readFile(imagePath, (err, data) => {
+                if (err) {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('Not Found');
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'image/png' });
+                    res.end(data);
+                }
+            });
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        }
+    } else {
+        res.writeHead(405, { 'Content-Type': 'text/plain' });
+        res.end('Method Not Allowed');
+    }
 });
 
+const port = 3000;
 server.listen(port, () => {
-    console.log(`서버가 실행됩니다. http://localhost:${port}`);
+    console.log(`Server is listening at http://localhost:${port}`);
 });
