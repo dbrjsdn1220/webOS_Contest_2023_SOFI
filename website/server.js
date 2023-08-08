43383e8105482558a7a92917d69b7fa5a8b38e27
YuHanul <dbrjsdn1220@gmail.com>
2023-08-08 18:23:00 +0900

    Update server.js

--- website/server.js
+++ website/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = 3000;
 
-// 정적 파일을 제공하기 위해 Express의 static middleware를 사용합니다.
-// public 폴더 아래에 있는 정적 파일들을 제공합니다.
 app.use(express.static(path.join(__dirname, '/')));
 
 app.listen(port, () => {
