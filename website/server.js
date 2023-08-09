2ad12e30593ecffbb0649b8224146e66088625d2
YuHanul <dbrjsdn1220@gmail.com>
2023-08-09 20:14:00 +0900

    저장된 데이터 표시 및 수정 기능 추가
    ChatGPT 엉아의 힘을 빌렸는데 안되는게 있음 공부해서 수정 필요해 보임.

--- website/server.js
+++ website/server.js
@@ -34,3 +34,29 @@ app.post('/save-data', (req, res) => {
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port}/start_sofi.html 에서 실행 중입니다.`);
 });
+
+//여기서부터 공부
+app.get('/get-data', (req, res) => {
+    res.json(data); // 저장된 데이터를 JSON 형태로 응답
+});
+
+app.put('/update-data', express.json(), (req, res) => {
+    const updatedData = req.body;
+    const updatedId = updatedData.id;
+  
+    const index = data.findIndex(item => item.id === updatedId);
+    if (index !== -1) {
+      data[index] = { ...data[index], ...updatedData };
+      fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+          console.error('데이터 업데이트 중 오류 발생:', err);
+          res.status(500).send('데이터를 업데이트하는 중 오류가 발생했습니다.');
+        } else {
+          console.log('데이터가 성공적으로 업데이트되었습니다.');
+          res.status(200).send('데이터가 성공적으로 업데이트되었습니다.');
+        }
+      });
+    } else {
+      res.status(404).send('해당 ID를 가진 데이터를 찾을 수 없습니다.');
+    }
+  });
