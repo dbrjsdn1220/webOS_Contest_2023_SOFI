786e2065653915641ac1fb3f417002e1607b5748
zmdmths <109254230+zmdmths@users.noreply.github.com>
2023-08-09 13:05:42 +0900

    test1

---
+++ webos_gpio_test/gpio.js
@@ -0,0 +1,15 @@
+function iframeSelect(selector)
+{
+    if(selector==1)
+    {
+        //음성인식? 
+    }
+    else if(selector==2)
+    {
+        //기기동작
+    }
+    else if(selector==3)
+    {
+        //카메라
+    }
+}
