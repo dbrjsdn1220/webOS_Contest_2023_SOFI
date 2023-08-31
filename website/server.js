8722888937706d1bf7d7c13a983eb88f6e321907
dbrjsdn1220 <70747418+dbrjsdn1220@users.noreply.github.com>
2023-08-31 13:31:29 +0900

    노드 js 서버에 이식함

--- website/server.js
+++ website/server.js
@@ -2,43 +2,41 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
-const bodyParser = require('body-parser')
+const port = 5556;
  
-//처음 접속 시, 연결할 사이트
+/*처음 접속 시, 연결할 사이트
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'start_sofi.html'));
 });
-app.use(express.static(path.join(__dirname, '/'))); //요청 시 모든 파일에 접근 가능하게 함.
+app.use(express.static(path.join(__dirname, '/'))); //요청 시 user.json에 접근 가능. */
+
 app.use(express.json());
- 
 let data = []; // 데이터를 저장할 배열
  
-//website/user.json의 파일을 읽어 data 배열에 저장. 해당 파일이 없다면 catch로 오류 구문 출력.
+//user.json의 파일을 읽어 data 배열에 저장. 해당 파일이 없다면 catch로 오류 구문 출력.
 try {
     data = JSON.parse(fs.readFileSync('user.json'));
 } catch (error) {
     console.error('기존 데이터를 가져오는 도중 오류 발생:', error);
 }
- 
+
+//클라이언트가 데이터를 읽고 싶을 때
 app.get('/getData', (req, res) => {
   res.json(data); // 저장된 데이터를 JSON 형태로 응답
 });
  
 //데이터를 생성
-app.post('/saveUser', (req, res) => {
+app.post('/saveData', (req, res) => {
     const {name, allergy} = req.body; //html에서 body: JSON.stringify였음. 해당 값을 저장.
     var userID=0;
+    
     checkID: //userID 설정을 위해서 값이 있는지 없는지 체크
-    for(i=0; data[i]; i++){
-      for(j=0; data[j]; j++){
-        if(data[j].id == userID){
-          userID++;
-          continue checkID;
-        }
+    for(j=0; data[j]; j++){
+      if(data[j].id == userID){
+        userID++;
+        continue checkID;
       }
-      break;
-    };
+    }
     const userData = {id: userID, name, allergy};
     data.push(userData); // data 배열에 userData push
  
@@ -54,7 +52,7 @@ app.post('/saveUser', (req, res) => {
 });
  
 //데이터 삭제
-app.delete('/deleteUser', (req, res) => {
+app.delete('/deleteData', (req, res) => {
   const user = req.body;
   var deleteUser = 0;
   for(i=0; data[i]; i++){
@@ -75,4 +73,6 @@ app.delete('/deleteUser', (req, res) => {
   });
 });
  
-app.listen(port, '0.0.0.0');
+app.listen(port, () => {
+  console.log('http://101.101.219.171:5556/ 서버가 열렸습니다.');
+});
