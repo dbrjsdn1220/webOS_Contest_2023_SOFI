3bd1f60ea70002c2f7bc898b9f5496ab65e903eb
dbrjsdn1220 <70747418+dbrjsdn1220@users.noreply.github.com>
2023-09-12 23:27:21 +0900

    ++해서 반복시키기 안되면 서버 따로 열어서 따로 처리하면 뚝딱이긴 함;

--- website/js/gpio.js
+++ website/js/gpio.js
@@ -1,5 +1,40 @@
 var bridge = new WebOSServiceBridge();
-var sentence = "";
+var url, params, sentence;
+
+function voiceStart() //음성인식 시작
+{ 
+  url = 'luna://com.webos.service.ai.voice/start';
+  params = {
+    "mode": "single",
+    "keywordDetect": false
+  };
+  bridge.call(url, JSON.stringify(params));
+}
+function voiceGetState() //음성인식 상태
+{
+  url = 'luna://com.webos.service.ai.voice/getState';
+  bridge.onservicecallback = getState;
+  params = {
+    "subscribe": true
+  };
+  bridge.call(url, JSON.stringify(params));
+}
+function voiceGetResponse() //음성인식 응답
+{
+  url = 'luna://com.webos.service.ai.voice/getResponse';
+  bridge.onservicecallback = getResponse;
+  params = {
+    "subscribe": true
+  };
+  bridge.call(url, JSON.stringify(params));
+}
+function voiceStop() //음성인식 종료
+{
+  url = 'luna://com.webos.service.ai.voice/stop';
+  params = {};
+  bridge.call(url, JSON.stringify(params));
+}
+
 
 function gpio_test()
 {
@@ -16,7 +51,7 @@ function gpio_test()
     "direction":"outHigh"
   }
   bridge.call(url, JSON.stringify(params));
-  delay(1000);
+  delay(10);
   var url = 'luna://com.webos.service.peripheralmanager/gpio/setDirection';
   var params={
     "pin":"gpio21", 
@@ -31,68 +66,67 @@ function delay(n){
   });
 }
 
+//음성인식
 function aiVoiceStart()
 {
-  //음성인식 시작
-  var url = 'luna://com.webos.service.ai.voice/start';
-  var params = {
-    "mode": "single",
-    "keywordDetect": false
-  };
-  bridge.call(url, JSON.stringify(params));
-
-  //음성인식 응답
-  url = 'luna://com.webos.service.ai.voice/getState';
-  bridge.onservicecallback = getState;
-  params = {
-    "subscribe": true
-  };
-  bridge.call(url, JSON.stringify(params));
+  voiceStart();
+  voiceGetState();
 }
 
+//음성인식 상태값
 function getState(msg) //추후에 가능하면 state없이 response 바로 받아오도록 시도
 {
   //서버로 로그 보내는 부분
-  fetch('http://101.101.219.171:5556/logCheck', {
+  fetch('http://101.101.219.171:5556/logCheckState', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json' //json 형태의 파일을 다룸.
     },
     body: msg
   })
-  .then(response => response.text()) //텍스트의 형태로
+  .then(response => response.text())
   .then(message => {
-    console.log(message);
-
-    if(JSON.parse(msg.state) == "answering") {
-      url = 'luna://com.webos.service.ai.voice/getResponse';
-      bridge.onservicecallback = getResponse;
-      params = {
-         "subscribe": true
-      };
-      bridge.call(url, JSON.stringify(params));
+    console.log("getState", message);
+    if(message == "thinking"){
+      voiceGetResponse();
     }
-  })
-  .catch(error => {
-    console.error('GetState:', error);
-  });  
-}
-
-function getResponse(msg) 
-{
-  //서버로 로그 보내는 부분
-  fetch('http://101.101.219.171:5556/logCheck', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json' //json 형태의 파일을 다룸.
-    },
-    body: msg
-  })
-  .then(response => response.text()) //텍스트의 형태로
-  .then(message => {
-    console.log(message);
   })
   .catch(error => {
     console.error('오류 발생:', error);
   });
 }
+
+//음성인식 응답값
+function getResponse(msg) 
+{
+  //서버로 로그 보내는 부분
+  fetch('http://101.101.219.171:5556/logCheckResponse', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' //json 형태의 파일을 다룸.
+    },
+    body: msg
+  })
+  .then(response => response.text())
+  .then(message => {
+    console.log("getResponse", message);
+    getSentence();
+  })
+  .catch(error => {
+    console.error('오류 발생:', error);
+  });
+}
+
+//음성인식 값 받아오기
+function getSentence() {
+  fetch('http://101.101.219.171:5556/getWords')
+  .then(response => response.text())
+  .then(words => {
+    console.log("words", words)
+    voiceStop();
+  })
+  .catch(error => {
+    console.error('오류 발생:', error);
+  });
+}
+
