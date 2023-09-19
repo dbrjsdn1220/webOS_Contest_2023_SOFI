a36e1e92c97612434d499185f1c6bfde6b1f4107
zmdmths <sksvlshzldh2@naver.com>
2023-09-19 21:00:48 +0900

    er

--- website/js/gpio.js
+++ website/js/gpio.js
@@ -25,6 +25,45 @@ function gpio_test()
   bridge.call(url, JSON.stringify(params));
 }
 
+
+function hasGetUserMedia() {
+  return !!(navigator.GetUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+}
+
+function c_test(){
+  var url = 'luna-send -n 1 -f luna://com.webos.service.camera2/startCapture';
+  var params={
+    "handle":6696,
+    "width": 640,
+    "height": 480,
+    "format": "JPEG",
+    "mode":"MODE_BURST",
+    "nimage":2,
+    "path":"/media/multimedia"
+  }
+  bridge.call(url, JSON.stringify(params));
+  /*
+  if (hasGetUserMedia()){
+    console.log("getUserMedia() is supported in your browser");
+  }
+  else{
+    console.log("getUserMedia() is not supported in your browser");
+  }
+  var video = document.querySelector("#videoElement");
+
+  if(navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({video:true, audio:true}).then(function(stream){
+      video.srcObject=stream;
+    })
+    .catch(function(error){
+      console.log("Something went wrong! " + error);
+    });
+  }
+  */
+}
+  
+
 function delay(n){
   return new Promise(function(resolve){
       setTimeout(resolve,n*1000);
