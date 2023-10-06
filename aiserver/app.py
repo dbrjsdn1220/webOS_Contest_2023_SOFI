f69bd807c2b3d3cec84bae7412a809de38ac68c0
KN-20 <2020112323@student.kyungnam.ac.kr>
2023-10-06 13:14:23 +0900

    aiserver 테스트용 파일

--- aiserver/app.py
+++ aiserver/app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/ImgSend', methods = ['POST', 'GET'])
 def ImgSend():
-    return 0
+    if request.method == 'POST':
+        test_file = request.form['test']
+        print(test_file)
+        return "POST TEST DATA: %s" %id
+    elif request.method == 'GET':
+        test_file = request.args.get('test')
+        print(type(test_file))
+        print(test_file)
+        return "GET TEST DATA: %s" %test_file
 
 @app.route('/')
 def test():
@@ -12,4 +20,4 @@ def test():
     return '<h1>Hello World<h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5501)
+    app.run(host='0.0.0.0', port=5501, debug=True)
